06fcf2cd1c9f0026bab04c5dc1460a6d5b8bcd4c
OddUlrich <vmoonodd@gmail.com>
2023-12-17 19:07:34 +0800

    More string printing in Hello world for execution golang in GoLand.

--- hello.go
+++ hello.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+/*
+Block-comment.
+*/
 func main() {
+	// Line-comment.
 	fmt.Println("Hello, World!")
+	fmt.Println("Good" + " " + "morning" + "!")
+
+	var format = "%s %d %s."
+
+	var date1 = "2021-10-12"
+	var index1 = 1
+	var action1 = "Say hello"
+
+	var s1 = fmt.Sprintf(format, date1, index1, action1)
+	fmt.Println(s1)
+
+	fmt.Printf(format, "2022-02-21", 2, "Print result")
+
 }
