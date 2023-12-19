cbd138fcfa6b1843cefce090986a675721a931a9
OddUlrich <vmoonodd@gmail.com>
2023-12-19 23:21:59 +0800

    Condition practice.

---
+++ condition.go
@@ -0,0 +1,129 @@
+package main
+
+import "fmt"
+
+func main() {
+	ifElse()
+	switchCase()
+	typeSwitch()
+	fallThroughSwitch()
+	selectCase()
+}
+
+func ifElse() {
+	var a int = 100
+
+	if a < 20 {
+		fmt.Printf("a 小于 20\n")
+	} else {
+		fmt.Printf("a 不小于 20\n")
+	}
+	fmt.Printf("a 的值为 : %d\n", a)
+
+}
+
+func switchCase() {
+	var grade string = "B"
+	var marks int = 90
+
+	switch marks {
+	case 90:
+		grade = "A"
+	case 80:
+		grade = "B"
+	case 50, 60, 70:
+		grade = "C"
+	default:
+		grade = "D"
+	}
+
+	switch {
+	case grade == "A":
+		fmt.Printf("优秀!\n")
+	case grade == "B", grade == "C":
+		fmt.Printf("良好\n")
+	case grade == "D":
+		fmt.Printf("及格\n")
+	case grade == "F":
+		fmt.Printf("不及格\n")
+	default:
+		fmt.Printf("差\n")
+	}
+	fmt.Printf("你的等级是 %s\n", grade)
+}
+
+func typeSwitch() {
+	var x interface{}
+
+	switch i := x.(type) {
+	case nil:
+		fmt.Printf(" x 的类型 :%T", i)
+	case int:
+		fmt.Printf("x 是 int 型")
+	case float64:
+		fmt.Printf("x 是 float64 型")
+	case func(int) float64:
+		fmt.Printf("x 是 func(int) 型")
+	case bool, string:
+		fmt.Printf("x 是 bool 或 string 型")
+	default:
+		fmt.Printf("未知型")
+	}
+
+	fmt.Println()
+}
+
+func fallThroughSwitch() {
+
+	switch {
+	case false:
+		fmt.Println("1、case 条件语句为 false")
+		fallthrough
+	case true:
+		fmt.Println("2、case 条件语句为 true")
+		fallthrough
+	case false:
+		fmt.Println("3、case 条件语句为 false")
+		fallthrough
+	case true:
+		fmt.Println("4、case 条件语句为 true")
+	case false:
+		fmt.Println("5、case 条件语句为 false")
+		fallthrough
+	default:
+		fmt.Println("6、默认 case")
+	}
+	fmt.Println()
+
+}
+
+func selectCase() {
+	// 定义两个通道
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	// 启动两个 goroutine，分别从两个通道中获取数据
+	go func() {
+		for {
+			ch1 <- "from 1"
+		}
+	}()
+	go func() {
+		for {
+			ch2 <- "from 2"
+		}
+	}()
+
+	// 使用 select 语句非阻塞地从两个通道中获取数据
+	for i := 0; i < 10; i++ {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		default:
+			// 如果两个通道都没有可用的数据，则执行这里的语句
+			fmt.Println("no message received")
+		}
+	}
+}
